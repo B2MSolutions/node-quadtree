f71b0688b908b903716b3d2674a5e0df073f6737
B2M Development <development@b2m-solutions.com>
2012-09-10 17:07:03 +0200

    trigger travis build

--- test/quadtree_test.js
+++ test/quadtree_test.js
@@ -19,7 +19,6 @@ describe('quadtree', function(){
       assert.equal(quadtree.encode({lng : 156.0, lat : 35.0}, 100), '1310333011323110333011323110333011323110333011323111231111111111111111111111111111111111111111111111');
     });
   });
-
   describe('#decode()', function(){
      it('should return expected for given quadtree precision 100', function() {
        assert.deepEqual(quadtree.decode('1310333011323110333011323110333011323110333011323111231111111111111111111111111111111111111111111111').origin, {lng : 156.0, lat : 35.0});
